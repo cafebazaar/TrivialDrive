3f70df5750ec24619ec4141a28d0bf83e36b79e8
shayan <shayanpourvatan@gmail.com>
2020-04-19 10:51:11 +0430

    Update AIDL based on new change on bazaar

--- app/src/main/java/com/example/android/trivialdrivesample/util/IabHelper.java
+++ app/src/main/java/com/example/android/trivialdrivesample/util/IabHelper.java
@@ -397,7 +397,7 @@ public class IabHelper {
             int apiVersion = 3;
             String packageName = mContext.getPackageName();
 
-            Bundle configBundle = mService.getPurchaseConfig(apiVersion, packageName, itemType);
+            Bundle configBundle = mService.getPurchaseConfig(apiVersion);
             if (configBundle != null && configBundle.getBoolean(INTENT_V2_SUPPORT)) {
                 logDebug("launchBuyIntentV2 for " + sku + ", item type: " + itemType);
                 launchBuyIntentV2(act, sku, itemType, requestCode, listener, extraData);
